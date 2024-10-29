b38015dd54369528053273ef1db6460c1060cd7a
WooSeulLee <dldntmf@naver.com>
2024-10-29 10:49:53 +0900

    2번째 테수투

---
+++ src/main/java/com/test/jenkins/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.test.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/")
+	public String getTest() {
+		return "2번째 테스트~";
+	}
+}
